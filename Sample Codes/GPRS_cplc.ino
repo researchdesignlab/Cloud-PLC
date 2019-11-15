5e5b65574676652b148c08140b17c8736957450b
researchdesignlab <sales@researchdesignlab.com>
2019-11-15 12:55:07 +0530

    GPRS Code

---
+++ Sample Codes/GPRS_cplc.ino
@@ -0,0 +1,19 @@
+#include <CPLC.h> 
+char data[] = "Hello RDL111!!";
+char* host = "mm.cloudmqtt.com";  //Your host ID
+//char* host = "52.3.184.147";
+char* port = "11000";            //Your port number
+char* username = "*****";        //Your username
+char* password = "******";       / Your password
+char* topic = "ethernet";
+//char *APN = "tp://airtelgprs.com"; //for airtel sim
+//char *APN = "internet"; //for Idea sim 
+char *APN = "TATA.DOCOMO.INTERNET"; //for DoCoMo sim 
+void setup(){
+Serial.begin(9600);
+Serial.println("Start..");
+CPLC_GPRS_Upload_MQTT(data, APN, host, port, username, password, topic);
+}
+void loop()
+{
+}
